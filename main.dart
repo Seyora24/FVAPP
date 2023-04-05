763d34370189553746bdc7810400f2b0981b8dc5
cahyono2003 <114041933+cahyono2003@users.noreply.github.com>
2023-04-05 17:15:31 +0700

    Add files via upload

--- main.dart
+++ main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
-import 'widgets/mitra.dart';
+// import './page/loginpage.dart';
+// import './page/registerpage.dart';
+import './page/registermitrapage.dart';
 
-void main() {
+void main(List<String> args) {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Mitra(),
+      home: RegisterMitra(),
     );
   }
 }
+
